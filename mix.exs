3c008acbdbb67f65686d79d4001aa03d260b9be4
Daniel Kukula <daniel.kuku@gmail.com>
2023-12-17 00:44:01 +0000

    v2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fitparser.MixProject do
   def project do
     [
       app: :fitparser,
-      version: "0.1.5",
+      version: "0.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

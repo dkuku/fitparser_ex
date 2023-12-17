d1339d6e173e51be71074c33dc5d75eff4d15271
Daniel Kukula <daniel.kuku@gmail.com>
2023-12-17 01:17:46 +0000

    add unsupported targets

--- lib/fitparser.ex
+++ lib/fitparser.ex
@@ -31,7 +31,8 @@ defmodule Fitparser.Native do
   unsupported = [
     "aarch64-unknown-linux-musl",
     "riscv64gc-unknown-linux-gnu",
-    "x86_64-unknown-linux-musl"
+    "x86_64-unknown-linux-musl",
+    "arm-unknown-linux-gnueabihf"
   ]
 
   use RustlerPrecompiled,
@@ -40,7 +41,7 @@ defmodule Fitparser.Native do
     version: version,
     base_url: "https://github.com/dkuku/fitparser_ex/releases/download/v#{version}",
     force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
-    targets: Enum.uniq(RustlerPrecompiled.Config.default_targets()) -- unsupported
+    targets: Enum.uniq(RustlerPrecompiled.Config.default_targets() -- unsupported)
 
   @doc """
   this function accepts binary fit file and returns the file converted to term

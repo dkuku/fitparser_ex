c4b465fcac4a870149feff8698ded6d8948073f3
Daniel Kukula <daniel.kuku@gmail.com>
2023-12-17 12:59:33 +0000

    0.2.2

--- checksum-Elixir.Fitparser.Native.exs
+++
@@ -1,8 +0,0 @@
-%{
-  "fitparser_native-v0.2.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:9004ad70efb83c5047a05f2451d85494ec6547e64b26c7d1a00f9c7c2cca9e48",
-  "fitparser_native-v0.2.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:79bcbf0d1dc51be22ec4cec17854793893ea127e855abcaa4ea97996fb2f62be",
-  "libfitparser_native-v0.2.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:1dce4c0b72c5921ff788c2f44bd5a76c7abc0d6a6cf29083336de5c241699dbf",
-  "libfitparser_native-v0.2.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:11e99c876ff1cfd2a1dd595221cfb66d3517b30d1f0e420a6953a4138eb44c6c",
-  "libfitparser_native-v0.2.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:eb33ca38dfd914e28c28f6987205a2ba093af74e8bc78edd375ee69f7d904c54",
-  "libfitparser_native-v0.2.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:08eaaf085018f092a03a5f969092c5a906588ed84df70b8a9d20f55747658e43",
-}
